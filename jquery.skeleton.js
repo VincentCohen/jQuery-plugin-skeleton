aeb80a10724245f59f94422064726e4fdb455529
Vincent Cohen <V.cohen90@gmail.com>
2013-05-28 13:18:05 +0300

    jquery.skeleton.js
    Skeleton

---
+++ jquery.skeleton.js
@@ -0,0 +1,49 @@
+(function($){
+
+$.fn.skeleton = function(options) {
+
+    // support multiple elements
+		if (this.length > 1){
+        
+        this.each(function() { 
+            $(this).skeleton(options) 
+        });
+
+        return this;
+    }
+
+    // private variables
+    var pOne = '';
+    var pTwo = '';
+    // ...
+
+    // private methods
+    var foo = function() {
+        // do something ...
+        console.log("foo");
+    }
+    // ...
+
+    // public methods        
+    this.initialize = function() {
+        // do something ...
+        console.log("init");
+
+        return this;
+    };
+
+    this.bar = function() {
+        // do something ...
+        console.log("bar");
+        console.log(this);
+    };
+
+    return this.initialize();
+}
+})(jQuery);
+
+var myPlugin = $('#foo').skeleton();
+skeleton.bar();
+
+var myPlugin2 = $('#bar').skeleton();
+skeleton.bar();
