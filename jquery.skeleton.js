25786708827bf8e50bb253ae9e37f3bd81e1667c
Mauricio Kruijer <mauricio.kruijer@gmail.com>
2013-05-28 14:59:58 +0200

    Provided examples and indentation fix
    no space but tabs

--- jquery.skeleton.js
+++ jquery.skeleton.js
@@ -1,40 +1,37 @@
 ;(function($, window, document, undefined){
-    var defaults = {
-        label: 'yeahhhhh'
-    };
+	var defaults = {
+		label: 'yeahhhhh',
+		color: 'green'
+	};
 
-    var optionss = {
-        "init": function (options) {
-            options = $.extend({}, defaults, options);
-            // Do whatever
-            console.log('init');
-        },
-        "bar": function (options) {
-            // Do whatever
-            console.log('boem!');
-        }
-    };
+	var optionss = {
+		"init": function (options) {
+			options = $.extend({}, defaults, options);
+			// Do whatever
+			console.log('init');
+			this.css('color', options.color);
+		},
+		"bar": function (options) {
+			// Do whatever
+			console.log('boem!');
+			options = $.extend({}, defaults, options);
+			this.css('color', options.color).text(this.text() + ' ' + options.label);
+		}
+	};
 
-    $.fn.skeleton = function(options) { 
-        var args = arguments;
-        var argss = Array.prototype.slice.call(args, 1);
+	$.fn.skeleton = function(options) { 
+		var args = arguments;
+		var argss = Array.prototype.slice.call(args, 1);
 
-        return this.each(function() {
-            var $this = $(this);  // Might make sense to ignore this and just pass `this` to the following things
-            if (optionss[options]) {
-                optionss[options].apply($this, argss);
-            }
-            else if (typeof options === "object" || !options) {
-                optionss.init.apply($this, args);
-            }
-            else {
-                $.error("options " + options + " does not exist on jQuery.pollServer");
-            }
-        });
-    };
+		return this.each(function() {
+			var $this = $(this);  // Might make sense to ignore this and just pass `this` to the following things
+			if (optionss[options]) {
+				optionss[options].apply($this, argss);
+			} else if (typeof options === "object" || !options) {
+				optionss.init.apply($this, args);
+			} else {
+				$.error("options " + options + " does not exist on jQuery.pollServer");
+			}
+		});
+	};
 })(jQuery, window, document);
-
-/*
-http://stackoverflow.com/a/13131363
-So to call init, you can either use $("#a").pluggg({stuff: "stuff"}); or $("#a").pluggg("init", {stuff: "stuff"});. To call myFunc, you'd use the second example for init, but use "myFunc". Does that all make sense?
-*/
