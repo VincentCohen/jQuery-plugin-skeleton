ea36a0e54a9b6bca752992a7cbd3411b22266f6f
Vincent Cohen <V.cohen90@gmail.com>
2013-05-28 15:43:22 +0300

    Update jquery.skeleton.js

--- jquery.skeleton.js
+++ jquery.skeleton.js
@@ -1,49 +1,35 @@
-(function($){
+;(function($, window, document, undefined){
+    var defaults = {
+        label: 'yeahhhhh'
+    };
 
-$.fn.skeleton = function(options) {
+    var optionss = {
+        "init": function (options) {
+            options = $.extend({}, defaults, options);
+            // Do whatever
+            console.log('init');
+        },
+        "bar": function (options) {
+            // Do whatever
+            console.log('boem!');
+        }
+    };
 
-    // support multiple elements
-		if (this.length > 1){
-        
-        this.each(function() { 
-            $(this).skeleton(options) 
+    $.fn.skeleton = function(options) { 
+        var args = arguments;
+        var argss = Array.prototype.slice.call(args, 1);
+
+        return this.each(function() {
+            var $this = $(this);  // Might make sense to ignore this and just pass `this` to the following things
+            if (optionss[options]) {
+                optionss[options].apply($this, argss);
+            }
+            else if (typeof options === "object" || !options) {
+                optionss.init.apply($this, args);
+            }
+            else {
+                $.error("options " + options + " does not exist on jQuery.pollServer");
+            }
         });
-
-        return this;
-    }
-
-    // private variables
-    var pOne = '';
-    var pTwo = '';
-    // ...
-
-    // private methods
-    var foo = function() {
-        // do something ...
-        console.log("foo");
-    }
-    // ...
-
-    // public methods        
-    this.initialize = function() {
-        // do something ...
-        console.log("init");
-
-        return this;
     };
-
-    this.bar = function() {
-        // do something ...
-        console.log("bar");
-        console.log(this);
-    };
-
-    return this.initialize();
-}
-})(jQuery);
-
-var myPlugin = $('#foo').skeleton();
-myPlugin.bar();
-
-var myPlugin2 = $('#bar').skeleton();
-myPlugin2.bar();
+})(jQuery, window, document);
